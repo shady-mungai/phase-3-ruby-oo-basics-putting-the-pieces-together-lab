c78fdfd01c42aa2ee22398080168b445c7760f82
shady-mungai <shadrackmungai10@gmail.com>
2023-02-12 20:18:58 +0300

    Completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
+=begin
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+  
+    def initialize(title, author, page_count, genre)
+      @title = title
+      @author = author
+      @page_count = page_count
+      @genre = genre
+    end
+  
+    def turn_page
+      @page_count += 1
+    end
+  end
+=end
 
+class Book
+	attr_reader :title
+	attr_accessor :author, :page_count , :genre
 
+	def initialize(title)
+		@title = title
+	end
+	
+	def turn_page
+       # @page_count += 1
+        puts "Flipping the page...wow, you read fast!"
+
+	end
+end
